be6e011d363e60e3528d3cab1d09f766284b5be1
Jaleesa Robinson <robinson.jaleesa42@gmail.com>
2024-04-24 20:11:09 -0400

    add a request handler to authenication controller

--- backend/index.js
+++ backend/index.js
@@ -9,26 +9,19 @@ const app = express();
 // Express Settings
 app.use(cookieSession({
     name: 'session',
-    sameSite: 'strict',
     keys: [ process.env.SESSION_SECRET ],
     maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }))
+
 app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
-}))
+    origin: `http://localhost:5000`,
+    credentials: true}))
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.use(cors())
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+
 
 // Controllers & Routes
-
-app.use(express.urlencoded({ extended: true }))
-
 app.use('/places', require('./controllers/places'))
 app.use('/users', require('./controllers/users'))
 app.use('/authentication', require('./controllers/authentication'))
--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -3,13 +3,25 @@ import { createContext, useEffect, useState } from "react";
 
 export const CurrentUser = createContext()
 
-function CurrentUserProvider({ children }){
+function CurrentUserProvider({ children }) {
+    const [currentUser, setCurrentUser] = useState(null)
+    useEffect (() => {
+        const getLoggedInUser = async () => {
+            let response = await fetch('http://localhost:5000/authenication/profile')
+            let user = await response.json()
+            setCurrentUser(user)
+        }
+        getLoggedInUser()
+}, [])
+}
 
     const [currentUser, setCurrentUser] = useState(null)
     useEffect(() => {
         const getLoggedInUser = async () => {
             let response = await fetch('http://localhost:5000/authenication/profile', {
-                credentials: 'include',
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
             })
             let user = await response.json()
             setCurrentUser(user)
@@ -22,6 +34,5 @@ function CurrentUserProvider({ children }){
             {children}
         </CurrentUser.Provider>
     )
-}
 
 export default CurrentUserProvider
